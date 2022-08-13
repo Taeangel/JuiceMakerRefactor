daacaab87dbb77f1bb3951bd332a897d36dba661
Taeangel <wngks2050@gmail.com>
2022-08-14 06:07:24 +0900

    Initial Commit

---
+++ JuiceMakerRefactor/JuiceMakerRefactorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMakerRefactorApp.swift
+//  JuiceMakerRefactor
+//
+//  Created by song on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct JuiceMakerRefactorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
